a73dac2bb8de2c57370ef893ee7d6513fe0af6cf
Amir Ismagilov <progamailism@gmail.com>
2020-10-03 08:49:19 +0500

    Create page.rb

---
+++ page.rb
@@ -0,0 +1,15 @@
+find_by
+
+class User < ApplicationRecord
+  validates :login, :email, presence: true
+ 
+  before_validation :ensure_login_has_a_value
+ 
+  private
+    def ensure_login_has_a_value
+      if login.nil?
+        self.login = email unless email.blank?
+      end
+    end
+end
+
